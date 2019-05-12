773370873197b4027e640aef4952f22c75e470dd
Chaz L <chaz6cl@live.com>
2019-05-12 10:31:41 +1000

    Fixed some output formats

--- src/Euclid.java
+++ src/Euclid.java
@@ -2,17 +2,24 @@
 public class Euclid {
 	public static void main(String[] args) 
 	{
-		gcdTable(20, 50);
+		int a = 7224;
+		int b = 5791;
+		
+		gcdTable(a, b); // function to solve for greatest common deviser in table form
+		
+		gcd(a, b); // function to solve for greatest common deviser in function form
+		
+		gcdExtended(a, b); // extended function to solve for the x, y coefficients of a and b
 	}
 	
 	public static int count = 0;
 	
     public static double gcd(int a, int b) {
     	
-    	System.out.println("if "+b+" = 0");
+    	System.out.print(b+" = 0");
     	
         if (b == 0) {
-        	System.out.println("true");
+        	System.out.println(" is true ");
         	System.out.println("result is "+a);
             System.out.println("in " + count + " steps");
             System.out.println();
@@ -22,9 +29,9 @@ public class Euclid {
             return a;
         }                     
         
-        System.out.println("false");
-        System.out.println("gcd("+b+", "+a+" mod "+b);
-        System.out.println("= gcd("+b+", "+a % b);
+        System.out.println(" is false ");
+        System.out.println("gcd("+b+", "+a+" mod "+b + ")");
+        System.out.println("= gcd("+b+", "+a % b + ")");
         
         count++;
         
@@ -48,5 +55,31 @@ public class Euclid {
         count++;
         
         return gcdTable(b, a % b);
+    }
+    
+    public static void gcdExtended(long a, long b) {
+        long x = 0, y = 1, lastx = 1, lasty = 0, temp;
+
+        while (b != 0)
+        {
+            long q = a / b;
+            long r = a % b;
+ 
+            a = b;
+            b = r;
+ 
+            temp = x;
+            x = lastx - q * x;
+            lastx = temp;
+ 
+            temp = y;
+            y = lasty - q * y;
+            lasty = temp;
+            
+            System.out.println("x: " + lastx + " y: " + lasty);
+        }
+
+        System.out.println("Coefficients are  x: "+ lastx +" y: "+ lasty);
+
     }
 }
